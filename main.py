ab7dd5478e84131f6bbf81fd4382d7ac2c28c1e5
pedro-loures <PLoures2@hotmail.com>
2022-11-04 14:32:20 -0300

    Basic test usage

---
+++ main.py
@@ -0,0 +1,10 @@
+# TODO LIST
+#01 TODO read json
+#02 TODO default output
+
+# Local Imports 
+import util as ut
+import recommender.trivial as triv
+
+# External Imports
+import json
--- util.py
+++ util.py
@@ -1,5 +1,13 @@
 
 
-RATINGS = "path\to\ratings.jsonl
-CONTENT = "path\to\content.jsonl
-TARGETS = "path\to\targets.jsonl
+
+# Debug
+debug_text = "test text"
+
+# Input paths 
+RATINGS = "path\\to\\ratings.jsonl"
+CONTENT = "path\\to\\content.jsonl"
+TARGETS = "path\\to\\targets.jsonl"
+
+# Output paths 
+RECOMENDATION = "results\\recommendations.txt"
